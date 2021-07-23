47a9a2d90cbec9710ad6196e681ee3cda46730e0
ramon <monchouam@gmail.com>
2021-07-23 11:11:27 -0500

    added comments

--- src/main/java/com/misiontic/examen4/Examen4.java
+++ src/main/java/com/misiontic/examen4/Examen4.java
@@ -1,31 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.misiontic.examen4;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 /**
+ * Resolving the test Class to contain the customSort method that receives as a
+ * parameter a vector of integers of even length, its job will be to return the
+ * ordered vector in the next way: -The first half of the values must be sorted
+ * ascending -The second half of the values must be sorted descending
  *
  * @author Ramon
  */
 public class Examen4 {
 
-    public static int[] ordenamientoPersonalizado(int[] vector) {
+    /**
+     * Receives a vector and sorts it, the first half is sorted ascending and
+     * the second half is sorted descending. To do it was invoked two
+     * implemented methods, ascendingOrder and descendingOrder. To fill the
+     * returned vector was used the System.arraycopy method, merging the two
+     * sorted parts inside one vector.
+     *
+     * @param vector
+     * @return sortedVector
+     */
+    public static int[] customSort(int[] vector) {
 
-        int[] vectorOrdenado = new int[vector.length];
-        int middle = vector.length/2;
-        int[] ascendente = ascendingOrder(vector, middle - 1);
-        int[] descendente = descendingOrder(vector, middle);
+        int[] sortedVector = new int[vector.length];
+        int middle = vector.length / 2;
+        int[] ascend = ascendingOrder(vector, middle - 1);
+        int[] descend = descendingOrder(vector, middle);
 
-        System.arraycopy(ascendente, 0, vectorOrdenado, 0, middle);
-        System.arraycopy(descendente, middle, vectorOrdenado, middle, middle);
-        return vectorOrdenado;
+        System.arraycopy(ascend, 0, sortedVector, 0, middle);
+        System.arraycopy(descend, middle, sortedVector, middle, middle);
+        return sortedVector;
     }
 
+    /**
+     * Sorts a vector from 0 index until a given index in ascending way using an
+     * implementation of the direct insert sort method.
+     *
+     * @param input : vector to be sorted
+     * @param end : index until the method will sort the vector
+     * @return A : sorted array. Being a copy of the input vector
+     */
     public static int[] ascendingOrder(int[] input, int end) {
         int[] A = new int[input.length];
         System.arraycopy(input, 0, A, 0, input.length);
@@ -43,6 +57,14 @@ public class Examen4 {
         return A;
     }
 
+    /**
+     * Sorts a vector from a given index until a given last index in descending
+     * way using an implementation of the direct insert sort method.
+     *
+     * @param input : vector to be sorted
+     * @param start : index from the method will be start to sort
+     * @return A : sorted vector
+     */
     public static int[] descendingOrder(int[] input, int start) {
         int[] A = new int[input.length];
         System.arraycopy(input, 0, A, 0, input.length);
@@ -60,6 +82,11 @@ public class Examen4 {
         return A;
     }
 
+    /**
+     * Shows in console a vector
+     *
+     * @param v : vector to be shown
+     */
     public static void print(int[] v) {
         for (int n : v) {
             System.out.print(n + " ");
--- src/main/java/com/misiontic/examen4/Main.java
+++ src/main/java/com/misiontic/examen4/Main.java
@@ -9,16 +9,15 @@ package com.misiontic.examen4;
  *
  * @author Ramon
  */
-
-public class Main {    
+public class Main {
 
     public static void main(String[] args) {
-        int[] vectorEntrada = {0, 2, 1, 65, 66, 78, 12, 11, 90, 13};       
-        //int [] vectorEntradaOrdenado = {0, 1, 2, 65, 66, 90, 78, 13, 12, 11};
-        int[] salida1 = Examen4.ordenamientoPersonalizado(vectorEntrada);
-         //int[]salida2= Examen4.descendingOrder(vectorEntrada, 5);
+        int[] inputVector = {0, 2, 1, 65, 66, 78, 12, 11, 90, 13};
+        //sortedVector = {0, 1, 2, 65, 66, 90, 78, 13, 12, 11};
+        int[] sortedVector = Examen4.customSort(inputVector);
+        //int[]salida2= Examen4.descendingOrder(inputVector, 5);
         System.out.println("\n");
-       Examen4.print(salida1);
+        Examen4.print(sortedVector);
     }
 
 }
